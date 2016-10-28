5c49ca4decb7fb257bb586f3d5606bf6157c73b7
Tiago Mergulhão <me@tmergulhao.com>
2016-10-28 17:31:48 -0500

    Initial Commit

---
+++ Sisyphus/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Sisyphus
+//
+//  Created by Tiago Mergulhão on 28/10/16.
+//  Copyright © 2016 Tiago Mergulhão. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
