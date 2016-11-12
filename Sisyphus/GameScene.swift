bbac48938ddfb099fb6ad26f59370f265f8b5f6e
Tiago Mergulhão <me@tmergulhao.com>
2016-11-12 11:54:16 -0500

    Random modularized insect

--- Sisyphus/GameScene.swift
+++ Sisyphus/GameScene.swift
@@ -19,46 +19,30 @@ extension CGFloat {
 
 class GameScene : Scene {
 
-	var cockroach : Cockroach?
-	var mite : Mite?
-	var fly : Fly?
-
-	func addItems () {
-
-		let cockroach = Cockroach()
-
-		cockroach.position = CGPoint(x: 0, y: 0)
-
-		self.cockroach = cockroach
-		addChild(cockroach)
-
-		let mite = Mite()
-
-		mite.position = CGPoint(x: -0.33*frame.width, y: 0)
-
-		self.mite = mite
-		addChild(mite)
-
-		let fly = Fly()
-
-		fly.position = CGPoint(x: 0.33*frame.width, y: 0)
-
-		self.fly = fly
-		addChild(fly)
-
-	}
+	var insect : Insect!
 
 	override func sceneDidLoad() {
 
 		backgroundColor = NSColor.white
 
-		addItems()
+		let index = Int(arc4random_uniform(UInt32(3)))
+
+		switch index {
+		case 1:
+			insect = Cockroach()
+		case 2:
+			insect = Mite()
+		case 3:
+			insect = Fly()
+		default:
+			insect = Fly()
+		}
+
+		insect.position = CGPoint(x: 0, y: 0)
+		addChild(insect)
 	}
 
 	override func update(_ currentTime: TimeInterval) {
-
-		let rand : Bool = drand48() > 0.8
-		var speed : CGFloat = 60
 
 		if (previousTime == 0) {
 			previousTime = currentTime
@@ -66,45 +50,7 @@ class GameScene : Scene {
 
 		let deltaTime = previousTime - currentTime
 
-		if keys.contains(.action) {
-			speed *= 2
-
-			cockroach?.state = .action
-			mite?.state = .action
-			fly?.state = .action
-		} else {
-			cockroach?.state = .idle
-			mite?.state = .idle
-			fly?.state = .idle
-		}
-
-		var _angle : Degrees?
-
-		switch keysMask & ~Keys.action.mask {
-		case Keys.combine([.down, .left, .right, .up]), Keys.combine([]): break
-		case Keys.combine([.left, .right, .up]), Keys.up.mask: _angle = 0
-		case Keys.combine([.down, .left, .up]), Keys.left.mask: _angle = 2 * 45
-		case Keys.combine([.down, .right, .up]), Keys.right.mask: _angle = -2 * 45
-		case Keys.combine([.left, .right, .down]), Keys.down.mask: _angle = -4 * 45
-		case Keys.combine([.left, .up]): _angle = 1 * 45
-		case Keys.combine([.left, .down]): _angle = 3 * 45
-		case Keys.combine([.right, .up]): _angle = -1 * 45
-		case Keys.combine([.right, .down]): _angle = -3 * 45
-		default: break
-		}
-
-		if let angle = _angle {
-			let time = CGFloat(deltaTime)
-			let direction = CGVector(dx: speed * time * sin(angle.radians), dy: speed * time * -cos(angle.radians))
-
-			let rotate = SKAction.rotate(toAngle: angle.radians, duration: 0.2, shortestUnitArc: rand)
-			let move = SKAction.move(by: direction, duration: 0.2)
-			let compound = SKAction.sequence([rotate, move])
-
-			cockroach?.run(compound)
-			mite?.run(compound)
-			fly?.run(compound)
-		}
+		insect.act(onKeys : keys, keyGuard : keyGuard, interval : deltaTime)
 
 		for entity in self.entities {
 			entity.update(deltaTime: deltaTime)
--- Sisyphus/Insect.swift
+++ Sisyphus/Insect.swift
@@ -53,6 +53,46 @@ class Insect : SKSpriteNode {
 		}
 	}
 
+	func act(onKeys keys : Set<Keys>, keyGuard : Set<Keys>, interval : TimeInterval) {
+
+		let rand : Bool = drand48() > 0.8
+		var speed : CGFloat = 60
+		let keysMask : UInt16 = Keys.combine(keys)
+
+		if keys.contains(.action) {
+			speed *= 3
+
+			state = .action
+		} else {
+			state = .idle
+		}
+
+		var _angle : Degrees?
+
+		switch keysMask & ~Keys.action.mask {
+		case Keys.combine([.down, .left, .right, .up]), Keys.combine([]): break
+		case Keys.combine([.left, .right, .up]), Keys.up.mask: _angle = 0
+		case Keys.combine([.down, .left, .up]), Keys.left.mask: _angle = 2 * 45
+		case Keys.combine([.down, .right, .up]), Keys.right.mask: _angle = -2 * 45
+		case Keys.combine([.left, .right, .down]), Keys.down.mask: _angle = -4 * 45
+		case Keys.combine([.left, .up]): _angle = 1 * 45
+		case Keys.combine([.left, .down]): _angle = 3 * 45
+		case Keys.combine([.right, .up]): _angle = -1 * 45
+		case Keys.combine([.right, .down]): _angle = -3 * 45
+		default: break
+		}
+
+		if let angle = _angle {
+			let time = CGFloat(interval)
+			let direction = CGVector(dx: speed * time * sin(angle.radians), dy: speed * time * -cos(angle.radians))
+
+			let rotate = SKAction.rotate(toAngle: angle.radians, duration: 0.2, shortestUnitArc: rand)
+			let move = SKAction.move(by: direction, duration: 0.2)
+			let compound = SKAction.sequence([rotate, move])
+
+			run(compound)
+		}
+	}
 
 	func changeState (from before : InsectState, to after : InsectState) {}
 }
