461e355ca2d79abe2c0913bf1b48f66ccb681bcc
Tiago Mergulhão <me@tmergulhao.com>
2016-11-12 00:03:50 -0500

    Basic game actions

--- Sisyphus/GameScene.swift
+++ Sisyphus/GameScene.swift
@@ -9,11 +9,22 @@
 import SpriteKit
 import GameplayKit
 
+typealias Degrees = CGFloat
+
+let π = CGFloat(M_PI)
+
+extension CGFloat {
+	var radians : CGFloat {
+		return π * self / 180.0
+	}
+}
+
 class GameScene: SKScene {
 
 	let fontName = "Computer Pixel-7"
 
 	var keys : Set<GameKeys> = []
+	var keysMask : UInt16 = 0
 
 	var entities : Array<GKEntity> = [GKEntity]()
 	var graphs : Dictionary<String, GKGraph> = [String : GKGraph]()
@@ -57,9 +68,9 @@ class GameScene: SKScene {
 
 		let label = SKLabelNode(fontNamed: fontName)
 
-		label.text = "Hello World!"
+		label.text = ""
 		label.fontColor = SKColor.black
-		label.fontSize = 30
+		label.fontSize = 100
 		label.horizontalAlignmentMode = .center
 		label.verticalAlignmentMode = .baseline
 
@@ -74,6 +85,9 @@ class GameScene: SKScene {
 
     override func update(_ currentTime: TimeInterval) {
 
+		let rand : Bool = drand48() > 0.5
+		var speed : CGFloat = 60
+
         if (previousTime == 0) {
             previousTime = currentTime
         }
@@ -81,13 +95,50 @@ class GameScene: SKScene {
 		let deltaTime = previousTime - currentTime
 
 		if keys.contains(.action) {
+			speed *= 2
+
+			label?.text = "Pressing spacebar"
+
 			cockroach?.state = .flying
 			mite?.state = .jumping
 			fly?.state = .flying
 		} else {
+			label?.text = ""
+
 			cockroach?.state = .idle
 			mite?.state = .idle
 			fly?.state = .idle
+		}
+
+		var _angle : Degrees?
+
+		switch keysMask & ~GameKeys.action.mask {
+		case GameKeys.combine([.down, .left, .right, .up]), GameKeys.combine([]): break
+		case GameKeys.combine([.left, .right, .up]), GameKeys.combine([.up]): _angle = 0
+		case GameKeys.combine([.down, .left, .up]), GameKeys.combine([.left]): _angle = 2 * 45
+		case GameKeys.combine([.down, .right, .up]), GameKeys.combine([.right]): _angle = -2 * 45
+		case GameKeys.combine([.left, .right, .down]), GameKeys.combine([.down]): _angle = -4 * 45
+		case GameKeys.combine([.left, .up]): _angle = 1 * 45
+		case GameKeys.combine([.left, .down]): _angle = 3 * 45
+		case GameKeys.combine([.right, .up]): _angle = -1 * 45
+		case GameKeys.combine([.right, .down]): _angle = -3 * 45
+		default: break
+		}
+
+		if let angle = _angle {
+			let time = CGFloat(deltaTime)
+			let direction = CGVector(dx: speed * time * sin(angle.radians), dy: speed * time * -cos(angle.radians))
+
+			Swift.print(sin(angle))
+			Swift.print(direction)
+
+			let rotate = SKAction.rotate(toAngle: angle.radians, duration: 0.2, shortestUnitArc: rand)
+			let move = SKAction.move(by: direction, duration: 0.2)
+			let compound = SKAction.sequence([rotate, move])
+
+			cockroach?.run(compound)
+			mite?.run(compound)
+			fly?.run(compound)
 		}
 
         for entity in self.entities {
